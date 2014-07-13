585dfb179bd7da6d429964d696fb5ff4efe4d2fc
tempest200903 <tempest200903@gmail.com>
2014-07-13 15:12:59 +0900

    mvn site

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,35 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.logger.MemoryLogger;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	static Logger myLogger = Logger.getLogger(App.class.getName());
+
+	static MemoryLogger memoryLogger = new MemoryLogger();
+
+	static void consumeMemory() {
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < 1000000; i++) {
+			list.add(App.class.getName());
+		}
+	}
+
+	public static void main(String[] args) {
+		myLogger.info("begin");
+		for (int i = 0; i < 10; i++) {
+			memoryLogger.log(Level.INFO, String.valueOf(i));
+			consumeMemory();
+		}
+		myLogger.info("end");
+	}
 }
